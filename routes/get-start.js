1b8afb5f1f3ce7025ba57294c72cfa0f9cdcf438
partizanen69 <oleksii.ablitsov@gmail.com>
2018-07-29 18:32:11 +0300

    b value for linear regression calculation implemented

--- routes/get-start.js
+++ routes/get-start.js
@@ -52,92 +52,141 @@ module.exports = {
 			});
 	},
 	calculate: (req, res) => {
-		Forecast.find({})
-			.sort({ weekISO: 1 })
-			.exec((err, objs) => {
-				objs.forEach((i, idx) => {
-					//calculate period number
+		const one = new Promise((resolve, reject) => {
+			Forecast.find()
+				.sort({ weekISO: 1 })
+				.exec((err, objs) => {
+					objs.forEach((i, idx) => {
+						//calculate period number
+						i.perNum = idx + 1;
+
+						//calculate weighted average
+						if (idx > 25 && idx < objs.length - 26) {
+							var a = objs[idx - 26].sales / 2;
+							var b = objs[idx + 26].sales / 2;
+							var c = 0;
+							for (var z = idx - 25; z < idx; z++) {
+								c += objs[z].sales;
+							}
+							var d = 0;
+							for (var z = idx + 25; z > idx; z--) {
+								d += objs[z].sales;
+							}
+							var weightedAvg = (a + b + c + d) / 52;
+							i.weightedAvg = weightedAvg;
+						} else {
+							i.weightedAvg = 'No data';
+						}
+
+						//calculate seasonal coefficient for weeks > 26
+						if (idx > 25 && idx < objs.length - 26) {
+							var { sales, weightedAvg } = objs[idx];
+							var seasCoef = sales / weightedAvg;
+							i.seasCoef = seasCoef;
+						}
+						if (idx >= objs.length - 26) {
+							var relValue = objs.find(
+								elem =>
+									elem.year ===
+										objs[idx].year - 1 &&
+									elem.weekNum === objs[idx].weekNum
+							);
+							i.seasCoef = relValue.seasCoef;
+						}
+					});
+					resolve(objs);
+				});
+		});
+
+		const two = objs => {
+			//calculate seasonal coefficient first 26 weeks
+			objs.forEach((i, idx) => {
+				if (idx < 26) {
+					var relValue = objs.find(
+						elem =>
+							elem.year === objs[idx].year + 1 &&
+							elem.weekNum === objs[idx].weekNum
+					);
+					i.seasCoef = relValue.seasCoef;
+				}
+			});
+			return Promise.resolve(objs);
+		};
+
+		const three = objs => {
+			//calculate cleared sales
+			objs.forEach((i, idx) => {
+				i.clearSales = i.sales / i.seasCoef;
+				i.xTimesY = i.perNum * i.clearSales;
+				i.xSqr = Math.pow(i.perNum, 2);
+			});
+			return Promise.resolve(objs);
+		};
+
+		const four = objs => {
+			//sum x*y
+			var c = objs.reduce((sum, item) => {
+				return sum + item.xTimesY;
+			}, 0);
+			console.log('c', c);
+
+			// average of sum of periods
+			var d =
+				objs.reduce((sum, item) => {
+					return sum + item.perNum;
+				}, 0) / objs.length;
+			console.log('d', d);
+
+			//average cleared sales
+			var e =
+				objs.reduce((sum, item) => {
+					return sum + item.clearSales;
+				}, 0) / objs.length;
+			console.log('e', e);
+
+			//sum of x^2
+			var f = objs.reduce((sum, item) => {
+				return sum + item.xSqr;
+			}, 0);
+			console.log('f', f);
+			console.log('objs[199].xSqr', objs[199].xSqr);
+			console.log('objs[199].perNum', objs[199].perNum);
+
+			g = objs.length * d * e;
+			h = objs.length * Math.pow(d, 2);
+			b = (c + g) / (f - h);
+
+			console.log('b', b);
+
+			return Promise.resolve(objs);
+		};
+
+		one.then(two)
+			.then(three)
+			.then(four)
+			.then(objs => {
+				objs.forEach((item, idx) => {
 					Forecast.findOneAndUpdate(
-						{ _id: objs[idx]._id },
-						{ perNum: idx + 1 },
+						{ _id: item._id },
+						{
+							perNum: item.perNum,
+							weightedAvg: item.weightedAvg,
+							seasCoef: item.seasCoef,
+							clearSales: item.clearSales,
+						},
 						(err, doc) => {
 							if (err) console.log(err);
 						}
 					);
-
-					//calculate weighted average
-					if (idx > 25 && idx < objs.length - 26) {
-						var a = objs[idx - 26].sales / 2;
-						var b = objs[idx + 26].sales / 2;
-						var c = 0;
-						for (var i = idx - 25; i < idx; i++) {
-							c += objs[i].sales;
-						}
-						var d = 0;
-						for (var i = idx + 25; i > idx; i--) {
-							d += objs[i].sales;
-						}
-						var weightedAvg = (a + b + c + d) / 52;
-						Forecast.findOneAndUpdate(
-							{ _id: objs[idx]._id },
-							{ weightedAvg: weightedAvg },
-							(err, doc) => {
-								if (err) console.log(err);
-							}
-						);
-					} else {
-						Forecast.findOneAndUpdate(
-							{ _id: objs[idx]._id },
-							{ weightedAvg: 'No data' },
-							(err, doc) => {
-								if (err) console.log(err);
-							}
-						);
-					}
-
-					//calculate seasonal coefficient
-					if (idx > 25 && idx < objs.length - 26) {
-						var { sales, weightedAvg } = objs[idx];
-						var seasCoef = sales / weightedAvg;
-						Forecast.findOneAndUpdate(
-							{ _id: objs[idx]._id },
-							{ seasCoef: seasCoef },
-							(err, doc) => {
-								if (err) console.log(err);
-							}
-						);
-					}
-					if (idx < 26) {
-						Forecast.find({
-							year: objs[idx].year + 1,
-							weekNum: objs[idx].weekNum,
-						}).exec((err, docs) => {
-							Forecast.findOneAndUpdate(
-								{ _id: objs[idx]._id },
-								{ seasCoef: docs[0].seasCoef },
-								(err, doc) => {
-									if (err) console.log(err);
-								}
-							);
-						});
-					}
-					if (idx >= objs.length - 26) {
-						Forecast.find({
-							year: objs[idx].year - 1,
-							weekNum: objs[idx].weekNum,
-						}).exec((err, docs) => {
-							Forecast.findOneAndUpdate(
-								{ _id: objs[idx]._id },
-								{ seasCoef: docs[0].seasCoef },
-								(err, doc) => {
-									if (err) console.log(err);
-								}
-							);
-						});
-					}
 				});
 			});
+
 		res.redirect('/get-start');
+
+		// one.then(two)
+		// 	.then(three)
+		// 	.then(four)
+		// 	.catch(err => console.log(err.message));
 	},
 	clear: (req, res) => {
 		Forecast.remove({}, (err, result) => {
